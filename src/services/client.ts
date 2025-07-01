9164319dfeed8f6abdc4f70c7e8537d06b09ae06
kaladin <335095@niuitmo.ru>
2025-07-01 18:47:07 +0300

    pages5

--- src/services/client.ts
+++ src/services/client.ts
@@ -10,7 +10,7 @@ export const getTonClient = (network: Network) => {
   } else {
     client = new TonClient({
       endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
-      apiKey: 'ab70221ff1418fc7d9f931cf4d3220041fac2a7be6dc4bfc78ea070bb4ef5606',
+      apiKey: '0a5ed4bac006e02ff45b9978049612c0b73d3e60962ae8dfffd106d0ff90bbeb',
     })
 
     return client
